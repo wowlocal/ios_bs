640b85486563d93f8ab45ab1eecea479d1ce329d
Micci <kh.misha.9@icloud.com>
2023-05-15 21:27:18 +0300

    messenger

---
+++ messenger/Sources/messenger.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import networking
+
+public func getMessages() {
+	sendNetworkRequest()
+}
+
+import payments
+
+public func sendMoney() {
+	doTransaction()
+}
