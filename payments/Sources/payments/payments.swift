b4f162a48ff3ff4f13fcac11cd6d83df93cbaebe
Micci <kh.misha.9@icloud.com>
2023-05-15 21:27:29 +0300

    payments

---
+++ payments/Sources/payments/payments.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import networking
+
+public func doTransaction() {
+	sendNetworkRequest()
+	print("transaction completed")
+}
